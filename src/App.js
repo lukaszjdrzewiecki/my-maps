8987eb3c1b44c74358186321780ef24a2ee00913
Łukasz Drzewiecki <lukasz.j.drzewiecki@gmail.com>
2018-12-06 22:47:23 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Datamap from './Datamap.js';
+
+class App extends Component {
+  render() {
+    return (
+        <Datamap />
+    );
+  }
+}
+
+export default App;
